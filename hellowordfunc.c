059c60693a9a08d2fb97c63623789a306713a475
izzdotnet2018 <izzdotnet@gmail.com>
2021-09-03 01:13:13 +0800

    new upload

---
+++ hellowordfunc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+char func1(){
+    printf("hellow world \n");
+}
+
+int main(){
+    printf("hellow world \n");
+    func1();
+}
---
+++ project.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+int max(int a);
+
+int main(){
+	//int a;
+	printf("%d\n",max(23));
+	
+}
+
+int max(int a){
+	int result;
+	
+	result = a * 10;
+	
+	return result;
+}
